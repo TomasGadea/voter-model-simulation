2bc1eefad7e998f4f15764734ed6435df06e8d0b
Tomás Gadea <01tomas.gadea@gmail.com>
2020-09-23 20:30:02 +0200

    just a grid xd

--- simulation.py
+++ simulation.py
@@ -6,45 +6,43 @@ Created on Wed Sep 23rd 2020
 """
 
 import pygame
-import time
 import numpy as np
+import time
 
 WIDTH, HEIGHT = 800, 800
 nX, nY = 10, 10
-xSize = WIDTH/nX
-ySize = HEIGHT/nY
+xSize = WIDTH / nX
+ySize = HEIGHT / nY
 
 pygame.init()  # Initialize PyGame
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])  # Set size of screen
 
-BG_COLOR = (10, 10, 10)  # Define background color
-LIVE_COLOR = (255, 255, 255)
-DEAD_COLOR = (128, 128, 128)
+BG_COLOR = (255, 255, 255)  # Define background color
 
-
-pauseRun = False
-
+pause = False
 running = True
-while running:
 
-    # newStatus = np.copy(status) # Copy status
+while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            pauseRun = not pauseRun
-
-        """mouseClick = pygame.mouse.get_pressed()
-        if sum(mouseClick) > 0:
-            posX, posY = pygame.mouse.get_pos()
-            x, y = int(np.floor(posX/xSize)), int(np.floor(posY/ySize))
-            #newStatus[x,y] = np.abs(newStatus[x,y]-1)
-            newStatus[x, y] = not mouseClick[2]"""
-
     screen.fill(BG_COLOR)  # Clean background
+
+    for x in range(0, nX):
+        for y in range(0, nY):
+
+            poly = [(x*xSize, y*ySize),
+                    ((x+1)*xSize, y*ySize),
+                    ((x+1)*xSize, (y+1)*ySize),
+                    (x*xSize, (y+1)*ySize)]
+
+            pygame.draw.polygon(screen, (200, 200, 200), poly, 1)
+
+    time.sleep(0.1)
+    pygame.display.flip()
 
 
 pygame.quit()
